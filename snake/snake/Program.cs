dd72045ce753ae9bac427964a0a519d42d8a8ee7
1@1.ru <1@1.ru>
2017-03-12 00:26:01 +0300

    second lesson - functions

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            int x1 = 5;
+            int y1 = 2;
+            char sym1 = '*';
+            draw(x1, y1, sym1);
+
+            int x2 = 2;
+            int y2 = 7;
+            char sym2 = '#';
+            draw(x2, y2, sym2);
             Console.ReadLine();
+
+        }
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
 
         }
     }
