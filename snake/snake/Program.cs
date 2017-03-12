73ebb29e2decb5d38a25816668e477a4eaf2ac93
1@1.ru <1@1.ru>
2017-03-12 15:07:00 +0300

    lesson 6

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,6 +16,29 @@ namespace snake
             point p2 = new point(2,7,'#');
             p2.draw();
 
+            List<int> num = new List<int>();
+
+            num.Add(0);
+            num.Add(1);
+            num.Add(2);
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+
+            num.RemoveAt(0);
+
+            List<point> listpoint = new List<point>();
+            listpoint.Add(p1);
+            listpoint.Add(p2);
+
+            point p3 = new point(1, 2, '%');
+            point p4 = new point(3, 5, '@');
+
+            listpoint.Add(p3);
+            listpoint.Add(p4);
+
             Console.ReadLine();
 
         }
